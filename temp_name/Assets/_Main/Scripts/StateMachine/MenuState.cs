98447587350e86084bd2d367a7965ca8bda023ba
Dawid Majdanik <dawidmajdanik96@gmail.com>
2020-02-06 21:54:26 +0100

    StateMachine

---
+++ temp_name/Assets/_Main/Scripts/StateMachine/MenuState.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuState : BaseState, IMenuView, IMovement
+{
+
+    private InputController.InputValues inputs;
+
+    public override void InitState(GameController gameController)
+    {
+        base.InitState(gameController);
+        this.gameController.UIController.MenuView.listener = this;
+        this.gameController.UIController.MenuView.ShowView();
+        this.gameController.SceneReferences.MenuTerrain.SetActive(true);
+#if UNITY_EDITOR
+        SetGameState();
+#endif
+    }
+
+    public override void UpdateState(GameController gameController)
+    {
+        this.gameController.MovementController.MovementUpdate(inputs);
+    }
+
+    public override void DeinitState(GameController gameController)
+    {
+        base.DeinitState(gameController);
+        this.gameController.UIController.MenuView.listener = null;
+        this.gameController.UIController.MenuView.HideView();
+        this.gameController.SceneReferences.MenuTerrain.SetActive(false);
+    }
+
+    public void SetGameState()
+    {
+        gameController.ChangeState(new GameState());
+    }
+
+    #region INTERFACES
+    public void UpdateAxis(InputController.InputValues inputValues)
+    {
+        inputs = inputValues;
+    }
+    #endregion
+}

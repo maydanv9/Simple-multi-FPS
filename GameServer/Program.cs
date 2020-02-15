18a93d3c04315a64463b6b8b9925f1fd478e3185
Dawid Majdanik <dawidmajdanik96@gmail.com>
2020-02-15 23:42:03 +0100

    WIP: Server initial commit

---
+++ GameServer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GameServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Title = "Game Server";
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GameServer/Server.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace GameServer
+{
+    class Server
+    {
+        public static int maxPlayers { get; private set;  }
+        public static int port { get; private set;  }
+        private static TcpListener tcpListener;
+        public void Start(int _maxPlayers, int _port)
+        {
+            maxPlayers = _maxPlayers;
+            port = _port;
+
+            tcpListener = new 
+        }
+    }
+}
